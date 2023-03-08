f5a4d00be4f3ff0d30d06022589642b0cb187317
Mads Sørensen <s215805@student.dtu.dk>
2023-03-08 09:40:11 +0100

    changed hough circles parameters to detect balls on the robot course

--- vision/main.py
+++ vision/main.py
@@ -11,13 +11,24 @@ while True:
     if not ret:
         break
 
-    blurred = cv.GaussianBlur(frame, (11, 11), 0)
-    grayFrame = cv.cvtColor(blurred, cv.COLOR_BGR2GRAY)
+    # TODO: Explore different blurs
+    blurFrame = cv.GaussianBlur(frame, (11, 11), 0)
+    #blurFrame = cv.medianBlur(frame, 11)
+    grayFrame = cv.cvtColor(blurFrame, cv.COLOR_BGR2GRAY)
 
-    # Play around with the arguments to get it to work
-    circles = cv.HoughCircles(grayFrame, cv.HOUGH_GRADIENT, 1, 20, param1=100, param2=30, minRadius=20, maxRadius=50)
+    # These configurations works okay with the current course setup
+    circles = cv.HoughCircles(image=grayFrame,
+                              method=cv.HOUGH_GRADIENT,
+                              dp=1,
+                              minDist=30,
+                              param1=20, # gradient value used in the edge detection
+                              param2=17, # lower values allow more circles to be detected (false positives)
+                              minRadius=6, # limits the smallest circle to this size (via radius)
+                              maxRadius=8 # similarly sets the limit for the largest circles
+                              )
 
     if circles is not None:
+        # Round the values to unsigned integers
         circles = np.uint16(np.around(circles))
         for i in circles[0, :]:
             # draw the outer circle
