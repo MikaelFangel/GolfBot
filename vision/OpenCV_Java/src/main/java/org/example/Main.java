4ff405d4df31f5a3cb1e40dcf333fa52528978a2
Frederik Petersen <frederik.greve@gmail.com>
2023-03-09 19:23:52 +0100

    Made function for getting the circles for each frame

--- vision/OpenCV_Java/src/main/java/org/example/Main.java
+++ vision/OpenCV_Java/src/main/java/org/example/Main.java
@@ -8,6 +8,7 @@ import org.opencv.imgproc.Imgproc;
 import org.opencv.videoio.VideoCapture;
 
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -19,34 +20,51 @@ public class Main {
 
         VideoCapture capture = new VideoCapture();
 
-        capture.open(2); // Might need to be changed
-
-        long start = System.currentTimeMillis();
+        capture.open(0); // Might need to be changed
 
         while (true) {
             if (capture.isOpened()) {
-                Mat frame = new Mat();
-                capture.read(frame);
+                ArrayList<double[]> circleCoords = getCirclesFromFrame(capture);
 
-                //Converting the image to Gray
-                Mat frame_gray = new Mat();
-                Imgproc.cvtColor(frame, frame_gray, Imgproc.COLOR_RGBA2GRAY);
-
-                Mat frame_blur = new Mat();
-                Imgproc.medianBlur(frame_gray, frame_blur, 5);
-
-                if (!frame.empty()) {
-                    Mat circles = new Mat();
-                    Imgproc.HoughCircles(frame_blur, circles, Imgproc.HOUGH_GRADIENT, 1, 30, 20, 17, 6, 8);
-
-                    if (!circles.empty()) {
-                        for (int i = 0; i < circles.width(); i++) {
-                            double[] center = circles.get(0, i);
-                            System.out.println(center[0] + ", " + center[1]);
-                        }
-                    }
+                for (double[] circle : circleCoords) {
+                    System.out.println("X: " + circle[0] + ", Y: " + circle[1]);
                 }
             }
         }
+    }
+
+    public static ArrayList<double[]> getCirclesFromFrame(VideoCapture capture) {
+        Mat frame = new Mat();
+        capture.read(frame);
+
+        //Converting the image to Gray and blur it
+        Mat frame_gray = new Mat();
+        Imgproc.cvtColor(frame, frame_gray, Imgproc.COLOR_RGBA2GRAY);
+        Mat frame_blur = new Mat();
+        Imgproc.medianBlur(frame_gray, frame_blur, 5);
+
+
+        ArrayList<double[]> circleCoords = new ArrayList<>();
+
+        if (!frame.empty()) {
+            // Get circles from frame
+            Mat circles = new Mat();
+            Imgproc.HoughCircles(frame_blur, circles, Imgproc.HOUGH_GRADIENT, 1, 30, 20, 17, 6, 8);
+
+            // Add circle coords to return arraylist
+            if (!circles.empty()) {
+                for (int i = 0; i < circles.width(); i++) {
+                    double[] center = circles.get(0, i);
+
+                    double[] coords = new double[2];
+                    coords[0] = center[0];
+                    coords[1] = center[1];
+
+                    circleCoords.add(coords);
+                }
+            }
+        }
+
+        return circleCoords;
     }
 }
