69a41f15441bc6a2f5933f99d302740bc7759bd5
Silja <91070829+yech199@users.noreply.github.com>
2023-03-14 23:24:39 +0100

    Initial implementation of API (not working...)

--- robot/main.go
+++ robot/main.go
@@ -51,16 +51,56 @@ func getMotorHandle(port string) *ev3dev.TachoMotor {
 }
 
 func (s *motorServer) RunMotor(ctx context.Context, in *pBuff.MotorRequest) (*pBuff.StatusReply, error) {
-	motor := getMotorHandle(in.GetMotorType())
+	motor, err := getMotorHandle(in.GetMotorType())
 	motor.SetSpeedSetpoint(int(in.GetMotorSpeed()))
 	motor.Command(run)
 
-	return &pBuff.StatusReply{ReplyMessage: true}, nil
+	return &pBuff.StatusReply{ReplyMessage: true}, err
 }
 
 func (s *motorServer) StopMotor(ctx context.Context, in *pBuff.MotorRequest) (*pBuff.StatusReply, error) {
-	motor := getMotorHandle(in.GetMotorType())
+	motor, err := getMotorHandle(in.GetMotorType())
 	motor.Command(stop)
 
-	return &pBuff.StatusReply{ReplyMessage: true}, nil
+	return &pBuff.StatusReply{ReplyMessage: true}, err
+}
+
+func (s *motorServer) DriveStraight(ctx context.Context, in *pBuff.MultipleMotors) *pBuff.StatusReply {
+	var motors [3]*ev3dev.TachoMotor
+
+	for i, request := range in.Motor {
+		motor, err := getMotorHandle(request.GetMotorType())
+		if err == nil {
+			return &pBuff.StatusReply{ReplyMessage: false, ErrorMsg: err.Error()}
+		}
+		motors[i] = motor
+		motor.SetSpeedSetpoint(int(request.GetMotorSpeed()))
+	}
+
+	for _, motor := range motors {
+		motor.Command(run)
+	}
+
+	return &pBuff.StatusReply{ReplyMessage: true}
+}
+
+func (s *motorServer) Turn(ctx context.Context, in *pBuff.MultipleMotors) *pBuff.StatusReply {
+	for _, request := range in.Motor {
+		motor, err := getMotorHandle(request.GetMotorType())
+		motor.SetSpeedSetpoint(int(request.GetMotorSpeed()))
+		motor.Command(run)
+	}
+
+	return &pBuff.StatusReply{ReplyMessage: true, ErrorMsg: err}
+
+}
+
+func (s *motorServer) StopMultiple(ctx context.Context, in *pBuff.MultipleMotors) *pBuff.StatusReply {
+	for _, request := range in.Motor {
+		motor, err := getMotorHandle(request.GetMotorType())
+		motor.SetSpeedSetpoint(int(request.GetMotorSpeed()))
+		motor.Command(run)
+	}
+
+	return &pBuff.StatusReply{ReplyMessage: true, ErrorMsg: err}
 }
