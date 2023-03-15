8d312d5148e169c5c0422f685654d96b4b569c9e
Silja <91070829+yech199@users.noreply.github.com>
2023-03-15 09:24:57 +0100

    Simplified protocol and its implementation
    Simplified the protocol by reducing the amount of services, and
    reflected this in the implementation of it in main.go

--- robot/main.go
+++ robot/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"net"
 
@@ -27,6 +28,11 @@ type motorServer struct {
 	pBuff.UnimplementedMotorsServer
 }
 
+type motorRequest struct {
+	request *pBuff.MotorRequest
+	motor   *ev3dev.TachoMotor
+}
+
 func main() {
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
@@ -47,60 +53,29 @@ func getMotorHandle(port string) (*ev3dev.TachoMotor, error) {
 
 func isRunning(motor *ev3dev.TachoMotor) bool {
 	speed, _ := motor.Speed()
-	if speed != 0 {
-		return true
-	}
-	return false
+
+	return speed != 0
 }
 
-func (s *motorServer) RunMotor(ctx context.Context, in *pBuff.MotorRequest) (*pBuff.StatusReply, error) {
-	motor, err := getMotorHandle(in.GetMotorType())
-	motor.SetSpeedSetpoint(int(in.GetMotorSpeed()))
-	motor.Command(run)
-
-	return &pBuff.StatusReply{ReplyMessage: true}, err
-}
-
-func (s *motorServer) StopMotor(ctx context.Context, in *pBuff.MotorRequest) (*pBuff.StatusReply, error) {
-	motor, err := getMotorHandle(in.GetMotorType())
-	motor.Command(stop)
-
-	return &pBuff.StatusReply{ReplyMessage: true}, err
-}
-
-func (s *motorServer) DriveStraight(ctx context.Context, in *pBuff.MultipleMotors) (*pBuff.StatusReply, error) {
-	var motors [3]*ev3dev.TachoMotor
+func (s *motorServer) RunMotors(ctx context.Context, in *pBuff.MultipleMotors) (*pBuff.StatusReply, error) {
+	// var motors [3]*ev3dev.TachoMotor
+	var motorRequests [3]motorRequest
 
 	for i, request := range in.Motor {
 		motor, err := getMotorHandle(request.GetMotorType())
 		if err == nil {
 			return &pBuff.StatusReply{ReplyMessage: false}, err
 		}
-		motors[i] = motor
+		motorRequests[i] = motorRequest{request: request, motor: motor}
 		motor.SetSpeedSetpoint(int(request.GetMotorSpeed()))
 	}
 
-	for _, motor := range motors {
-		motor.Command(run)
-	}
-
-	return &pBuff.StatusReply{ReplyMessage: true}, nil
-}
-
-func (s *motorServer) Turn(ctx context.Context, in *pBuff.MultipleMotors) (*pBuff.StatusReply, error) {
-	var motors [3]*ev3dev.TachoMotor
-
-	for i, request := range in.Motor {
-		motor, err := getMotorHandle(request.GetMotorType())
-		if err == nil {
-			return &pBuff.StatusReply{ReplyMessage: false}, err
+	for _, motorRequest := range motorRequests {
+		motorRequest.motor.Command(run)
+		isRunning := isRunning(motorRequest.motor)
+		if !isRunning {
+			return &pBuff.StatusReply{ReplyMessage: false}, fmt.Errorf("motor %s is not running", motorRequest.request.MotorType)
 		}
-		motors[i] = motor
-		motor.SetSpeedSetpoint(int(request.GetMotorSpeed()))
-	}
-
-	for _, motor := range motors {
-		motor.Command(run)
 	}
 
 	return &pBuff.StatusReply{ReplyMessage: true}, nil
