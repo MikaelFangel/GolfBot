3b3ae0cd287b55e1d94c1489888fc2532a1fed43
Mads Sørensen <s215805@dtu.dk>
2023-04-26 11:14:32 +0200

    added arraylist

--- remote/src/main/java/courseObjects/Course.java
+++ remote/src/main/java/courseObjects/Course.java
@@ -2,9 +2,20 @@ package courseObjects;
 
 import org.opencv.core.Point;
 
+import java.util.List;
+
 public class Course {
     public final double length = 167.0, width = 122.0;
     public Point topLeft, topRight, bottomLeft, bottomRight;
-    public Ball[] balls;
+
+    private List<Ball> balls;
     public Robot robot;
+
+    public List<Ball> getBalls() {
+        return balls;
+    }
+
+    public void setBalls(List<Ball> balls) {
+        this.balls = balls;
+    }
 }
