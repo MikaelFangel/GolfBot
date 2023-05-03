2e99fd2ecda3dcbdee651ce046ca52a67b2bcb65
Silja <91070829+yech199@users.noreply.github.com>
2023-05-03 12:31:14 +0200

    Changed default ip
    Co-authored-by: asbp <s194582@student.dtu.dk>

--- remote/src/main/java/Main.java
+++ remote/src/main/java/Main.java
@@ -3,7 +3,7 @@ import exceptions.MissingArgumentException;
 public class Main {
     public static void main(String[] args) throws InterruptedException, MissingArgumentException {
         if (args.length < 1) {
-            throw new MissingArgumentException("Please provide an IP and port number (e.g 192.168.0.97:50051)");
+            throw new MissingArgumentException("Please provide an IP and port number (e.g 192.168.1.12:50051)");
         }
 
         RobotController controller = new RobotController(args[0]);
