490a0e011727187993655b8454be2bbbb398bfbd
Silja <91070829+yech199@users.noreply.github.com>
2023-06-07 16:20:14 +0200

    Added class a class representing the cross obstacle

---
+++ remote/src/main/java/courseObjects/Cross.java
@@ -0,0 +1,17 @@
+package courseObjects;
+
+import org.opencv.core.Point;
+
+import java.util.List;
+
+public class Cross {
+    private List<Point> endPoints;
+
+    public List<Point> getEndPoints() {
+        return endPoints;
+    }
+
+    public void setEndPoints(List<Point> endPoints) {
+        this.endPoints = endPoints;
+    }
+}
