b0dd1cd20bb8ed6dbcf35f93afba269dfa58e4f7
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-06-12 14:53:06 +0200

    added a method to check if line is intersecting a circle

--- remote/src/main/java/RoutingController.java
+++ remote/src/main/java/RoutingController.java
@@ -1,14 +1,19 @@
 import courseObjects.Course;
+import courseObjects.Cross;
 import org.opencv.core.Point;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class RoutingController {
-    private Course course;
-    private List<Route> fullRoute = new ArrayList<Route>();
+    private final Course course;
+    private final List<Route> fullRoute = new ArrayList<>();
     private Route currentRoute;
 
+
+    public RoutingController(Course course) {
+        this.course = course;
+    }
     //
     public void driveRoutes () {
 
@@ -19,6 +24,24 @@ public class RoutingController {
         //if ( /*TODO*/ ) {
 
         //}
+    }
+
+    /**
+     * This method checks whether a given line calculated from two points is intersecting a circle.
+     * @param from the starting point of the line
+     * @param to the ending point of the line
+     * @param circleCenter the center point of the circle (eg. course.getCross().getMiddle())
+     * @param circleRadius the radius of the circle to check if intersecting  (eg. course.getLongestSide() / 2)
+     * @return true if the line is intersecting with the circle
+     */
+    public boolean lineIsIntersectingCircle(Point from, Point to, Point circleCenter, double circleRadius) {
+        // Calculate the line between two points
+        double a = (from.y - to.y) / (from.x - to.x);
+        double b = from.y - a * from.x;
+
+        // Calculate the distance from the center of the circle
+        double dist = Math.abs(a*circleCenter.x + b - circleCenter.y) / Math.sqrt(Math.pow(a, 2) + 1);
+        return dist < circleRadius;
     }
 
     // Clear planned routes
