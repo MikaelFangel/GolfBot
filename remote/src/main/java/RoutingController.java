96a516ed58ea3d329ed3a1166c903384c9b2e4cd
asbp <s194582@student.dtu.dk>
2023-06-12 14:01:48 +0200

    Added RoutingController.java

---
+++ remote/src/main/java/RoutingController.java
@@ -0,0 +1,33 @@
+import courseObjects.Course;
+import org.opencv.core.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RoutingController {
+    private Course course;
+    private List<Route> fullRoute = new ArrayList<Route>();
+    private Route currentRoute;
+
+    //
+    public void driveRoutes () {
+
+    }
+
+    //
+    public void planRoute(Point from, Point to) {
+        if ( /*TODO*/ ) {
+
+        }
+    }
+
+    // Clear planned routes
+    public void clearFullRoute() {
+        fullRoute.clear();
+    }
+
+    // Stop ongoing route
+    public void stopCurrentRoute() {
+
+    }
+}
