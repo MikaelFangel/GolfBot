e96f3575f7abaab5f0586909244d19fa38c14360
Mikael Fangel <34864484+MikaelFangel@users.noreply.github.com>
2023-06-13 12:45:20 +0200

    expanded enum

--- remote/src/main/java/vision/BallPickupStrategy.java
+++ remote/src/main/java/vision/BallPickupStrategy.java
@@ -1,5 +1,14 @@
 package vision;
 
 public enum BallPickupStrategy {
-    FREE, BORDER, CORNER, CROSS
+    FREE,
+    BORDER_TOP,
+    BORDER_BOTTOM,
+    BORDER_RIGHT,
+    BORDER_LEFT,
+    CORNER_TOP_RIGHT,
+    CORNER_TOP_LEFT,
+    CORNER_BOTTOM_RIGHT,
+    CORNER_BOTTOM_LEFT,
+    CROSS
 }
